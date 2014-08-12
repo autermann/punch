d3ee54c99dc22e3ef19fcc2a0ec2b5e41aab228b
Christian Autermann <christian@autermann.org>
2014-08-13 01:27:38 +0200

    initial commit

---
+++ bin/punch-cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var cli = require('cli');
+var path = require("path")
+var punch = require('../lib/punch')
+
+cli.setApp("./package.json")
+	.enable("help", "version", "glob");
+
+
+var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+
+cli.parse({
+	file: [ "f", "Punch data file", "path", path.join(home, ".punch.json") ]
+}, ["in", "out", "status", "report"]);
+
+function checkArgLength(length) {
+	if (cli.argc !== length) { cli.getUsage(1); }
+}
+
+
+cli.main(function() {
+	var options = {file: this.options.file};
+	switch(this.command) {
+		case 'in':
+			checkArgLength(1);
+			options.what = this.args[0];
+			return punch.in(options);
+		case 'out':
+			checkArgLength(0);
+			return punch.out(options);
+		case 'report':
+			checkArgLength(0);
+			return punch.report(options);
+		case 'status':
+			checkArgLength(0);
+			return punch.status(options);
+	}
+});
